d64da1bf7664f786defced2acbd2bd388e40f96b
Yu-Jen Lin <peacedove5210@gmail.com>
2012-06-22 12:19:48 +0800

    Initial Commit

---
+++ SmartBlurFilter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SmartBlurFilter
+//
+//  Created by Yu-Jen Lin on 6/22/12.
+//  Copyright (c) 2012 home. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
